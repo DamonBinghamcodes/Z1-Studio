387b8e8e2456f37b26fce795fd83405a4eb9d42b
Damon <damon.bingham@hotmail.com>
2025-04-16 23:19:30 +1000

    updates

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-menu.classList.toggle("open")
-icon.classList.toggle("open")
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.querySelector(".cta-button");
+    btn.addEventListener("click", () => {
+      window.location.href = "projects.html"; // or any internal section
+    });
+  });
+  
