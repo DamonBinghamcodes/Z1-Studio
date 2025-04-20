9622303e57a86c76c3da513db9412b725def2bdd
Damon <damon.bingham@hotmail.com>
2025-04-20 20:43:28 +1000

    update

---
+++ Public/script.js
@@ -0,0 +1,10 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.getElementById("hamburger");
+    const navbar = document.getElementById("navbar");
+  
+    hamburger.addEventListener("click", () => {
+      navbar.classList.toggle("show");
+    });
+  });
+  
